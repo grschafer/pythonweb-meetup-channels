e237a093c42d35c5a09b5e4e8848f8609d6c2a9e
Greg Schafer <grschafer@gmail.com>
2016-06-27 22:22:22 +0000

    Channels get-started chat example

--- channels_chat/routing.py
+++ channels_chat/routing.py
@@ -2,5 +2,7 @@ from channels.routing import route
 
 
 channel_routing = [
-    route("http.request", "core.consumers.http_consumer"),
+    route("websocket.connect", "core.consumers.ws_add"),
+    route("websocket.receive", "core.consumers.ws_message"),
+    route("websocket.disconnect", "core.consumers.ws_disconnect"),
 ]
